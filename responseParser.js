1780a701419f49ef2d1f31ac2d25336df39e152a
Lyall <me@lyall.lol>
2024-11-26 21:43:47 +0000

    ignore

--- responseParser.js
+++ responseParser.js
@@ -1,6 +1,7 @@
 // custom function to parse ai responses
 // for example, if your system prompt is setup in a way so the ai responds in a certain format, you can parse it here
 module.exports = (message) => {
+    // console.log(message);
     return {
         ignored: message.match(/^ignored/i),
         ignoredReason: message.match(/^ignored:\s*(.*)/i)?.[1],
